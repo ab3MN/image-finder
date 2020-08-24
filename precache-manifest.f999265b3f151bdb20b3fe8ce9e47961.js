8638a6fbe639799c236e9ae3ea121523d4697534
ab3MN <ab3mn3@gmail.com>
2020-08-24 16:36:23 +0300

    Updates

---
+++ precache-manifest.f999265b3f151bdb20b3fe8ce9e47961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da416df89d4b2e97b66fecabe0ed96ff",
+    "url": "/image-finder/index.html"
+  },
+  {
+    "revision": "8a0442c22ac457276f44",
+    "url": "/image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "b66e9f1eccf3b1ce5e88",
+    "url": "/image-finder/static/css/main.eed19a60.chunk.css"
+  },
+  {
+    "revision": "8a0442c22ac457276f44",
+    "url": "/image-finder/static/js/2.167bf299.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-finder/static/js/2.167bf299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66e9f1eccf3b1ce5e88",
+    "url": "/image-finder/static/js/main.275b8e77.chunk.js"
+  },
+  {
+    "revision": "fd2eaa904c1b97683958",
+    "url": "/image-finder/static/js/runtime-main.87c7f485.js"
+  }
+]);
